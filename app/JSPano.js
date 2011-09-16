f5316bb93753d7dd133e058bbd5852d7e48c88ba
André Fiedler <kontakt@visualdrugs.net>
2011-09-16 11:43:25 +0000

    init

---
+++ app/JSPano.js
@@ -0,0 +1,126 @@
+var camera, scene, renderer;
+
+var fov = 70,
+texture_placeholder,
+isUserInteracting = false,
+onMouseDownMouseX = 0, onMouseDownMouseY = 0,
+lon = 0, onMouseDownLon = 0,
+lat = 0, onMouseDownLat = 0,
+phi = 0, theta = 0;
+
+init();
+animate();
+
+function init() {
+
+	var container, mesh;
+
+	container = document.getElementById('container');
+
+	camera = new THREE.Camera(fov, window.innerWidth / window.innerHeight, 1, 1100);
+
+	scene = new THREE.Scene();
+
+	mesh = new THREE.Mesh(new THREE.SphereGeometry(500, 60, 40), new THREE.MeshBasicMaterial({ 
+        map: THREE.ImageUtils.loadTexture('http://mrdoob.github.com/three.js/examples/textures/2294472375_24a3b8ef46_o.jpg') 
+    }));
+	mesh.scale.x = -1;
+	scene.addObject( mesh );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	container.appendChild( renderer.domElement );
+
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+	document.addEventListener( 'DOMMouseScroll', onDocumentMouseWheel, false);
+
+}
+
+function onDocumentMouseDown( event ) {
+
+	event.preventDefault();
+
+	isUserInteracting = true;
+
+	onPointerDownPointerX = event.clientX;
+	onPointerDownPointerY = event.clientY;
+
+	onPointerDownLon = lon;
+	onPointerDownLat = lat;
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	if ( isUserInteracting ) {
+
+		lon = ( onPointerDownPointerX - event.clientX ) * 0.1 + onPointerDownLon;
+		lat = ( event.clientY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
+
+	}
+}
+
+function onDocumentMouseUp( event ) {
+
+	isUserInteracting = false;
+
+}
+
+function onDocumentMouseWheel( event ) {
+
+	// WebKit
+
+	if ( event.wheelDeltaY ) {
+
+		fov -= event.wheelDeltaY * 0.05;
+
+	// Opera / Explorer 9
+
+	} else if ( event.wheelDelta ) {
+
+		fov -= event.wheelDelta * 0.05;
+
+	// Firefox
+
+	} else if ( event.detail ) {
+
+		fov += event.detail * 1.0;
+
+	}
+
+	camera.projectionMatrix = THREE.Matrix4.makePerspective( fov, window.innerWidth / window.innerHeight, 1, 1100 );
+	render();
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+
+	lat = Math.max( - 85, Math.min( 85, lat ) );
+	phi = ( 90 - lat ) * Math.PI / 180;
+	theta = lon * Math.PI / 180;
+
+	camera.target.position.x = 500 * Math.sin( phi ) * Math.cos( theta );
+	camera.target.position.y = 500 * Math.cos( phi );
+	camera.target.position.z = 500 * Math.sin( phi ) * Math.sin( theta );
+
+	/*
+	// distortion
+	camera.position.x = - camera.target.position.x;
+	camera.position.y = - camera.target.position.y;
+	camera.position.z = - camera.target.position.z;
+	*/
+
+	renderer.render( scene, camera );
+
+}
